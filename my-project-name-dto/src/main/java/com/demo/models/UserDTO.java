beffb36b96a338c7771ba2d8ed719bd64d4d4dad
Davi Oliveira <davi.oliveira@pst.asseco.com>
2020-03-19 12:56:17 +0000

    few more http methods samples

---
+++ my-project-name-dto/src/main/java/com/demo/models/UserDTO.java
@@ -0,0 +1,11 @@
+package com.demo.models;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    private static final long serialVersionUID = -6975162011805908635L;
+
+    private String name;
+    private String age;
+}
--- my-project-name-services/src/main/java/com/demo/controllers/UserController.java
+++ my-project-name-services/src/main/java/com/demo/controllers/UserController.java
@@ -1,20 +1,23 @@
 package com.demo.controllers;
 
 import com.demo.entity.User;
+import com.demo.models.UserDTO;
 import com.demo.services.UserService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8")
+@RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8",
+        consumes = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8")
+@Api(tags = "User")
 public class UserController {
 
     private UserService userService;
@@ -24,12 +27,36 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user")
-    public ResponseEntity<?> returnUser(@RequestParam("name") String name) {
+    @GetMapping
+    public ResponseEntity<?> returnUserByName(@RequestParam("name") String name) {
         Optional<User> user = Optional.ofNullable(userService.findUserByName(name));
         if(user.isPresent()){
             return new ResponseEntity<>(user, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> returnUsers() {
+        Optional<List<User>> users = Optional.ofNullable(userService.findAll());
+        if(users.isPresent()){
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveUser(@RequestBody UserDTO userDTO) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Integer id) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
